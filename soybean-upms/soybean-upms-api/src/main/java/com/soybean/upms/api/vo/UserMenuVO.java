c32029ce78197a3fe8beb556637335a26f213286
dongdongjie <384168902@qq.com>
2025-12-30 15:39:55 +0800

    todo

---
+++ soybean-upms/soybean-upms-api/src/main/java/com/soybean/upms/api/vo/UserMenuVO.java
@@ -0,0 +1,25 @@
+package com.soybean.upms.api.vo;
+
+import com.soybean.upms.api.vo.SysMenuVO;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 用户菜单VO（包含路由和首页信息）
+ *
+ * @author soybean
+ */
+@Data
+public class UserMenuVO {
+
+    /**
+     * 菜单路由列表
+     */
+    private List<SysMenuVO> routes;
+
+    /**
+     * 首页路径
+     */
+    private String home = "home";
+}
---
+++ soybean-user/soybean-user-api/src/main/java/com/soybean/user/api/vo/UserInfoVO.java
@@ -0,0 +1,98 @@
+package com.soybean.user.api.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.soybean.common.core.annotation.Desensitization;
+import com.soybean.common.core.desensitization.EmailDesensitization;
+import com.soybean.common.core.desensitization.NameDesensitization;
+import com.soybean.common.core.desensitization.PhoneDesensitization;
+import com.soybean.user.api.enums.SysUserStatusEnum;
+import com.soybean.user.api.enums.SysUserGenderEnum;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 用户信息VO（包含基本信息、角色和权限）
+ *
+ * @author soybean
+ */
+@Data
+public class UserInfoVO {
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    @Desensitization(NameDesensitization.class)
+    private String username;
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+    /**
+     * 手机号
+     */
+    @Desensitization(PhoneDesensitization.class)
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    @Desensitization(EmailDesensitization.class)
+    private String email;
+
+    /**
+     * 状态：0->禁用；1->启用
+     */
+    private SysUserStatusEnum status;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
+
+    /**
+     * 性别
+     */
+    private SysUserGenderEnum gender;
+
+    /**
+     * 头像地址
+     */
+    private String avatar;
+
+    /**
+     * 最后登录IP
+     */
+    private String loginIp;
+
+    /**
+     * 最后登录时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime loginDate;
+
+    /**
+     * 角色列表
+     */
+    private List<String> roles;
+
+    /**
+     * 权限列表
+     */
+    private List<String> permissions;
+}
